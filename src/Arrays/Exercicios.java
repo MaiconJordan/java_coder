1756752f10f5ad01e4c193d6f24e8123c366bcfc
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2021-08-25 18:58:09 -0300

    about Arrays
    mais sobre arrays

---
+++ src/Arrays/Exercicios.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Exercicios {
+    public static void main(String[] args) {
+        double[] notasDoAlunoA = new double[3];
+
+        notasDoAlunoA[0] = 7;
+        notasDoAlunoA[1] = 8;
+        notasDoAlunoA[2] = 9;
+
+
+        System.out.println(Arrays.toString(notasDoAlunoA));
+        System.out.println(notasDoAlunoA[0]);
+
+    }
+}
