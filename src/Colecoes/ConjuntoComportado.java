90180a476832d5a11d94d0c2aa6056eb3fa446bb
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2021-08-27 19:08:06 -0300

    set
    conjunto set

---
+++ src/Colecoes/ConjuntoComportado.java
@@ -0,0 +1,38 @@
+package Colecoes;
+
+import DesafioClasses.EqualsHashCode;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+    public static void main(String[] args) {
+        // Set<String> lista = new HashSet<String>();
+        TreeSet<String> lista = new TreeSet<>(); // forma ordenada da lista
+
+        lista.add("Ana");
+        lista.add("Carlos");
+        lista.add("Paulo");
+        lista.add("Bia");
+
+        for(String nomes: lista){
+            System.out.println(nomes);
+        }
+
+        Set<Integer> nums = new HashSet<>();
+        nums.add(12);
+        nums.add(45);
+        nums.add(35);
+        nums.add(58);
+
+        for (int n: nums){
+            System.out.println(n);
+        }
+
+    }
+
+
+}
