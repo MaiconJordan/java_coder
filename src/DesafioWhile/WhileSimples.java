6963bacff93261573347cc2760c5b13cd07cbf63
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2021-09-01 08:44:35 -0300

    Contador While Simples
    Contador While Simples

---
+++ src/DesafioWhile/WhileSimples.java
@@ -0,0 +1,14 @@
+package DesafioWhile;
+
+public class WhileSimples {
+    public static void main(String[] args) {
+
+
+        int contadora = 1;
+
+        while (contadora <= 10) {
+            System.out.println(contadora);
+            contadora++;
+        }
+    }
+}
